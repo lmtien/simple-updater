18fe45d3adabd19406e3c01e371fc3d1edeedc48
Tidus Le <lmtien9116@gmail.com>
2014-05-21 15:19:26 +0800

    finish auto copy & delete

---
+++ Updater/frmMain.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Updater
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblInfos = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pgrUpdating = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblInfos);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(439, 25);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblInfos
+            // 
+            this.lblInfos.AutoSize = true;
+            this.lblInfos.ForeColor = System.Drawing.Color.White;
+            this.lblInfos.Location = new System.Drawing.Point(6, 7);
+            this.lblInfos.Name = "lblInfos";
+            this.lblInfos.Size = new System.Drawing.Size(35, 13);
+            this.lblInfos.TabIndex = 0;
+            this.lblInfos.Text = "label1";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.pgrUpdating);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 25);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(439, 14);
+            this.panel2.TabIndex = 1;
+            // 
+            // pgrUpdating
+            // 
+            this.pgrUpdating.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pgrUpdating.Location = new System.Drawing.Point(0, 0);
+            this.pgrUpdating.Name = "pgrUpdating";
+            this.pgrUpdating.Size = new System.Drawing.Size(439, 14);
+            this.pgrUpdating.TabIndex = 0;
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.WorkerReportsProgress = true;
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            this.backgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker_ProgressChanged);
+            this.backgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker_RunWorkerCompleted);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(439, 39);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Updater";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblInfos;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ProgressBar pgrUpdating;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+    }
+}
+
---
+++ Updater/frmMain.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Reflection;
+using System.Threading;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Updater
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            this.Text = "WizAdmin Updater v" + FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).ProductVersion;
+            backgroundWorker.RunWorkerAsync();
+        }
+
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                //Connect to server======================
+                backgroundWorker.ReportProgress(0, "Connecting...");
+
+                //Get current infos
+                SystemInfos current_info = JsonConvert.DeserializeObject<SystemInfos>(File.ReadAllText(@"system.lmt"));
+
+                //Get latest infos
+                SystemInfos latest_info = JsonConvert.DeserializeObject<SystemInfos>(File.ReadAllText(Path.Combine(current_info.server_url, @"system.lmt")));
+
+                //Compare versions======================
+                backgroundWorker.ReportProgress(0, "Comparing version...");
+                Version current_v = new Version(current_info.version);
+                Version latest_v = new Version(latest_info.version);
+                if (current_v.CompareTo(latest_v) >= 0)
+                {
+                    MessageBox.Show("Nothing to update, you are using the latest version!", "Information");
+                    return;
+                }
+
+                //Estimate time======================
+                backgroundWorker.ReportProgress(0, "Estimating time...");
+
+                //Get Job list
+                Jobs jobs = JsonConvert.DeserializeObject<Jobs>(File.ReadAllText(Path.Combine(current_info.server_url, "jobs.lmt")));
+
+                //delegate for progress bar
+                pgrUpdating.Invoke((MethodInvoker)delegate
+                {
+                    pgrUpdating.Maximum = jobs.copy.Count + jobs.delete.Count;
+                });
+
+                //Update======================
+                backgroundWorker.ReportProgress(0, "Updating...");
+
+                //Do Copy jobs
+                int progress = 0;
+                foreach (string cp in jobs.copy)
+                {
+                    backgroundWorker.ReportProgress(progress, "Updating: " + cp);
+                    File.Copy(Path.Combine(current_info.server_url, cp), cp, true);
+                    progress++;
+                }
+
+                //Do Delete jobs
+                foreach (string dl in jobs.delete)
+                {
+                    backgroundWorker.ReportProgress(progress, "Updating: " + dl);
+                    File.Delete(dl);
+                    progress++;
+                }
+                backgroundWorker.ReportProgress(progress, "Finished");
+
+                MessageBox.Show("Updated to latest version " + latest_info.version + " successfully!\n\nChange logs:\n" + latest_info.change_log, "Information");
+            }
+            catch
+            {
+                MessageBox.Show("Something wrongs, please try again later!", "Error");
+            }
+        }
+
+        private void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            pgrUpdating.Value = e.ProgressPercentage;
+            lblInfos.Text = e.UserState.ToString();
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (backgroundWorker.IsBusy)
+            {
+                if (MessageBox.Show("Are you sure you want to close?", "Confirmation", MessageBoxButtons.YesNo) == DialogResult.No)
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+
+        private void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+    public class Jobs
+    {
+        public List<string> copy { get; set; }
+        public List<string> delete { get; set; }
+    }
+
+    public class SystemInfos
+    {
+        public string version { get; set; }
+        public string server_url { get; set; }
+        public string change_log { get; set; }
+    }
+}
